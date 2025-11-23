88e457a8b2024b8ae39fc21de098398ee4cd1521
James Todd <jtodd@navigate360.com>
2025-11-23 09:57:26 -0800

    Fix clock position

--- index.js
+++ index.js
@@ -270,9 +270,9 @@ function drawClock(color) {
   
   // Medium font layout for 2-line clock: each line fits 2 digits
   // Hours: position to fit 2 digits in top half
-  ctx.fillText(hoursStr, 3, 1);     
+  ctx.fillText(hoursStr, 2, 1);     
   // Minutes: position to fit 2 digits + dot in bottom half  
-  ctx.fillText(minutesStr, 3, 8);   
+  ctx.fillText(minutesStr, 2, 8);   
 }
 
 // Clock mode!
