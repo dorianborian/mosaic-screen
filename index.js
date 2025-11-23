ed0a28f4251ca2478b827015a4bc06a890d03a05
James Todd <jtodd@navigate360.com>
2025-11-23 10:21:17 -0800

    Fix plasma logic

--- index.js
+++ index.js
@@ -490,59 +490,69 @@ function plasma({
   const clockRGB = hexToRGB(clockColor);
 
   return setInterval(() => {
-    for (var y = 0; y < h; y++) {
-      for (var x = 0; x < w; x++) {
-        var hue = hueShift + (plasma[y][x] % 1);
-        var rgb = HSVtoRGB(hue, plasma[y][x], plasma[y][x] * plasmaBrightness);
-        var pos = (y * w + x) * 4;
-        
-        ctx.pixels[pos] = rgb.r;
-        ctx.pixels[pos + 1] = rgb.g;
-        ctx.pixels[pos + 2] = rgb.b;
-        ctx.pixels[pos + 3] = 255;
-      }
-    }
-    
-    if (withClock) {
+    if (withClock && invert) {
+      clearScreen();
+      ctx.fillStyle = clockColor;
+      ctx.font = 'medium';
       const time = new Date();
       const minutes = time.getMinutes().toString().padStart(2, '0');
       let hours = time.getHours();
       let isPM = hours >= 12;
-      
       if (hours > 12) hours = hours - 12;
       if (hours === 0) hours = 12;
-
       const hoursStr = hours.toString().padStart(2, '0');
       const minutesStr = minutes + (isPM ? '.' : ' ');
-      
-      ctx.fillStyle = invert ? clockColor : bgColor;
-      ctx.font = 'medium';
       ctx.fillText(hoursStr, 2, 1);
       ctx.fillText(minutesStr, 2, 8);
       
-      if (invert) {
-        const imageData = ctx.getImageData(0, 0, w, h);
-        for (var y = 0; y < h; y++) {
-          for (var x = 0; x < w; x++) {
-            var pos = (y * w + x) * 4;
-            if (imageData.data[pos + 3] > 0) {
-              var hue = hueShift + (plasma[y][x] % 1);
-              var rgb = HSVtoRGB(hue, plasma[y][x], plasma[y][x] * plasmaBrightness);
-              ctx.pixels[pos] = rgb.r;
-              ctx.pixels[pos + 1] = rgb.g;
-              ctx.pixels[pos + 2] = rgb.b;
-            } else {
-              ctx.pixels[pos] = clockRGB.r;
-              ctx.pixels[pos + 1] = clockRGB.g;
-              ctx.pixels[pos + 2] = clockRGB.b;
-            }
+      const imageData = ctx.getImageData(0, 0, w, h);
+      for (var y = 0; y < h; y++) {
+        for (var x = 0; x < w; x++) {
+          var pos = (y * w + x) * 4;
+          if (imageData.data[pos + 3] > 0) {
+            var hue = hueShift + (plasma[y][x] % 1);
+            var rgb = HSVtoRGB(hue, plasma[y][x], plasma[y][x] * plasmaBrightness);
+            ctx.pixels[pos] = rgb.r;
+            ctx.pixels[pos + 1] = rgb.g;
+            ctx.pixels[pos + 2] = rgb.b;
+          } else {
+            ctx.pixels[pos] = bgRGB.r;
+            ctx.pixels[pos + 1] = bgRGB.g;
+            ctx.pixels[pos + 2] = bgRGB.b;
           }
         }
+      }
+    } else {
+      for (var y = 0; y < h; y++) {
+        for (var x = 0; x < w; x++) {
+          var hue = hueShift + (plasma[y][x] % 1);
+          var rgb = HSVtoRGB(hue, plasma[y][x], plasma[y][x] * plasmaBrightness);
+          var pos = (y * w + x) * 4;
+          ctx.pixels[pos] = rgb.r;
+          ctx.pixels[pos + 1] = rgb.g;
+          ctx.pixels[pos + 2] = rgb.b;
+          ctx.pixels[pos + 3] = 255;
+        }
+      }
+      
+      if (withClock) {
+        const time = new Date();
+        const minutes = time.getMinutes().toString().padStart(2, '0');
+        let hours = time.getHours();
+        let isPM = hours >= 12;
+        if (hours > 12) hours = hours - 12;
+        if (hours === 0) hours = 12;
+        const hoursStr = hours.toString().padStart(2, '0');
+        const minutesStr = minutes + (isPM ? '.' : ' ');
+        ctx.fillStyle = bgColor;
+        ctx.font = 'medium';
+        ctx.fillText(hoursStr, 2, 1);
+        ctx.fillText(minutesStr, 2, 8);
       }
     }
 
     hueShift = (hueShift + 0.01) % 1;
-  }, Math.round(1000 / 60));
+  }, Math.round(1000 / 60));}
 }
 
 function hexToRGB(hex) {
